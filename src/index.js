93509071fd5a0050e9c4929382d65dd1e23e06f7
AaLin-Git <aalin.box.it@gmail.com>
2023-02-01 15:12:25 +0100

    feat: finish morse decoder

--- src/index.js
+++ src/index.js
@@ -38,22 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
+  let result = "";
   for (let i = 0; i < expr.length; i += 10) {
     const binaryLetter = expr.slice(i, i + 10);
-    console.log(binaryLetter);
+    //console.log(binaryLetter);
     let morseResult = "";
-
-    for (let x = 0; x < binaryLetter.length; x += 2) {
-      let slisedLetter = binaryLetter.slice(x, x + 2);
-      //console.log(slisedLetter);
-      if (slisedLetter !== "00" && slisedLetter === "10") {
-        morseResult += ".";
-      } else if (slisedLetter !== "00" && slisedLetter === "11") {
-        morseResult += "-";
+    let str = "";
+    //let arr = [];
+    if (binaryLetter === "**********") {
+      result += " ";
+    } else {
+      for (let x = 0; x < binaryLetter.length; x += 2) {
+        let slisedLetter = binaryLetter.slice(x, x + 2);
+        //console.log(slisedLetter);
+        if (slisedLetter === "10") {
+          morseResult += ".";
+        } else if (slisedLetter === "11") {
+          morseResult += "-";
+        }
+        //console.log(morseResult);
       }
-      console.log(morseResult);
+
+      //console.log(morseResult)
+      Object.keys(MORSE_TABLE).map((morseLetter) => {
+        //console.log(morseResult);
+        //console.log(MORSE_TABLE[morseLetter])
+        if (morseLetter === morseResult) {
+          //console.log(Object.values[morseLetter])
+          // arr.push(Object.values(morseLetter));
+          // str = arr.join(' ');
+          result += MORSE_TABLE[morseLetter];
+          //console.log(result)
+        }
+        //console.log(str);
+      });
     }
   }
+  return result;
 }
 
 module.exports = {
